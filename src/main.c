24ed1439af663c901f66fe4e90b6c980041701a7
DinCraft <idiniyar2006@mail.ru>
2026-02-15 23:30:21 +0300

    some troubles with pow

--- src/main.c
+++ src/main.c
@@ -6,10 +6,5 @@ int main(int argc, char *argv[])
 {
   struct graph g;
   create_graph(&g, 4, 3);
-  vertex_add_edge(&g.verticies[0], LEFT);
-  vertex_add_edge(&g.verticies[0], TOP);
-  printf("%d\n", g.verticies[0].edges);
-  vertex_remove_edge(&g.verticies[0], LEFT);
-  printf("%d\n", g.verticies[0].edges);
   return 0;
 }
--- src/vertex.c
+++ src/vertex.c
@@ -1,17 +1,1 @@
 #include "../include/vertex.h"
-
-void vertex_set_xy(
-    int x, int y) {
-  v->x = x;
-  v->y = y;
-}
-
-void vertex_add_edge(
-    enum vertex_side side) {
-  v->edges |= (1 << side);
-}
-
-void vertex_remove_edge(
-    enum vertex_side side) {
-  v->edges &= ~(1 << side);
-}
